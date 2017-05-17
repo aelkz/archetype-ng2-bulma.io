a7b751f21675ff622f30bea3cd8041f1c53b0beb
Raphael Alex Silva Abreu <raphaelabreu@mec.gov.br>
2017-05-17 12:43:47 -0300

    fix

---
+++ src/app/shared/directives/modal/modal.directive.ts
@@ -0,0 +1,26 @@
+/**
+
+ <a class="button modal-button" data-target="#modal2">default</a>
+
+ <div id="modal2" class="modal">
+ <div class="modal-background"></div>
+ <div class="modal-card">
+ <header class="modal-card-head">
+ <p class="modal-card-title">Default modal</p>
+ </header>
+ <section class="modal-card-body">
+ <div class="content">
+ <h1>Stop! Don't shoot fire stick in space canoe! Cause explosive decompression!</h1>
+ <p>If rubbin' frozen dirt in your crotch is wrong, hey I don't wanna be right. Aww, it's true. I've been hiding it for so long. Can I use the gun? Yeah, and if you were the pope they'd be all, "Straighten your pope hat." And "Put on your good vestments."</p>
+ <p>But, like most politicians, he promised more than he could deliver. Is that a cooking show? <strong> Leela, are you alright?</strong> <em> You got wanged on the head.</em> For example, if you killed your grandfather, you'd cease to exist!</p>
+ </div>
+ </section>
+ <footer class="modal-card-foot">
+ <a class="button is-success">Save changes</a>
+ <a class="button">Cancel</a>
+ </footer>
+ </div>
+ <button class="modal-close"></button>
+ </div>
+
+ */
